7fb6d85fa50f4e7c924f583392b25df229afe1f5
tetsu-upstr <bodomu@gmail.com>
2020-09-14 06:41:57 +0900

    modify

--- array.js
+++ array.js
@@ -1,4 +1,5 @@
 // 主プログラム:Main
+let S = [];
 let K = 3;
 let M = 5;
 let R = Init(5, N, K);
@@ -8,24 +9,37 @@ while (R = 0) {
 }
 
 // 副プログラム:Init
-while ( L <= K && K <= M ) {
-    for (L = 1; L <= N; 1++) {
-        if ( L <= K ) {
-            s[L] = 1;
+function Init() {
+    while ( L <= K && K <= M ) {
+        for (L = 1; L <= N; 1++) {
+            if ( L <= K ) {
+                s[L] = 1;
+            }
+            s[L] = 0;
         }
-        s[L] = 0;
+        return 0;
     }
+    return -1;
 }
 
 // 副プログラム:Next
-let C = 0;
-let L = 1;
-let R = -1;
-
-while (L < N && R = -1) {
-    for (s[L] = 1) {
-
-
+function Next() {
+    let C = 0;
+    let L = 1;
+    let R = -1;
+    
+    while (L < N && R = -1) {
+        while (s[L] = 1) {
+            while(S[L + 1]) {
+                s[L] = 0;
+                s[L + 1] = 1;
+                Init(S, L-1, C);
+                R = 0;
+            }
+            C = C + 1;
+        }
         L = L + 1;
     }
+    
+    return R;
 }
