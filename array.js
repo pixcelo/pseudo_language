e5da990ff7586810016f29cfb47b53464105e56b
tetsu-upstr <bodomu@gmail.com>
2020-09-14 06:25:56 +0900

    second

--- array.js
+++ array.js
@@ -2,8 +2,30 @@
 let K = 3;
 let M = 5;
 let R = Init(5, N, K);
+
 while (R = 0) {
     R = Next(5, N);
 }
 
 // 副プログラム:Init
+while ( L <= K && K <= M ) {
+    for (L = 1; L <= N; 1++) {
+        if ( L <= K ) {
+            s[L] = 1;
+        }
+        s[L] = 0;
+    }
+}
+
+// 副プログラム:Next
+let C = 0;
+let L = 1;
+let R = -1;
+
+while (L < N && R = -1) {
+    for (s[L] = 1) {
+
+
+        L = L + 1;
+    }
+}
